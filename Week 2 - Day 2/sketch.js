8ae0be4ffd40b7fccb28b74a04fcd6c56577e08e
Billy-Spratt <N00192476@iadt.ie>
2022-02-09 13:08:58 +0000

    barchart with labels complete

--- Week 2 - Day 2/sketch.js
+++ Week 2 - Day 2/sketch.js
@@ -1,17 +1,30 @@
 let chartWidth = 400;
 let chartHeight = 400;
-let data = [230, 330, 120, 240, 390, 60]
+let data = [30, 70, 20];
+let dataNames = ["Apples", "Bananas", "Grapes"];
+let scaledData = [];
 let spacing = 10;
 let margin = 20;
 let availableWidth = chartWidth - (margin * 2) - (spacing * (data.length - 1));
 let barWidth = availableWidth / data.length;
 let sideNum = 10
 let sideSpacing = chartWidth / sideNum;
+let sideInc;
+
 
 
 
 function setup() {
     createCanvas(500, 500);
+    let maxValue = max(data);
+    sideInc = Math.round(maxValue) / sideNum;
+
+    for (let i = 0; i < data.length; i++) {
+        let tempVal = map(data[i], 0, max(data), 0, chartHeight)
+
+        scaledData.push(tempVal);
+
+    }
 }
 
 
@@ -19,18 +32,16 @@ function draw() {
     background(0);
     translate(50, 450);
 
-    fill(255, 0, 0);
-    noStroke();
-    textSize(20);
-    text("Graph", 0, 25);
+
 
     stroke(255);
     strokeWeight(2);
-    line(0, 0, 0, -400);
-    line(0, 0, 400, 0);
+    line(0, 0, 0, -chartHeight);
+    line(0, 0, chartWidth, 0);
 
     stroke(255, 200);
     strokeWeight(1);
+    //y-axis text
     for (let i = 0; i <= sideNum; i++) {
         stroke(255, 200);
         line(0, -sideSpacing * i, -sideNum, -sideSpacing * i);
@@ -38,19 +49,21 @@ function draw() {
         fill(255);
         textSize(13);
         textAlign(CENTER, CENTER);
-        text(sideSpacing * i, -30, -sideSpacing * i);
+        text(sideInc * i, -30, -sideSpacing * i);
     }
 
     translate(margin, 0);
 
+    //Bars
     for (let i = 0; i < data.length; i++) {
         fill(0, 255, 0);
         noStroke();
-        rect((barWidth + spacing) * i, 0, barWidth, -data[i]);
+        rect((barWidth + spacing) * i, 0, barWidth, -scaledData[i]);
         noStroke();
         fill(255);
-        textSize(12);
-        textAlign(LEFT, CENTER);
-        text(data[i], (barWidth + spacing) * i, -data[i] - 10);
+        textSize(22);
+        textAlign(CENTER, CENTER);
+        text(data[i], ((barWidth + spacing) * i) + barWidth / 2, -scaledData[i] - 10);
+        text(dataNames[i], ((barWidth + spacing) * i) + barWidth / 2, margin);
     }
 }
